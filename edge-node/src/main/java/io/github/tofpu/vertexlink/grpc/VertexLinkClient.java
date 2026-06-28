ec018460b8ebbd34b30b91f7cc7f6a1c31dfe5d3
Tofpu <tofpuu@gmail.com>
2026-06-29 02:06:20 +0300

    refactor(edge-node): use newly-introduced standardized gRPC client implementation

--- edge-node/src/main/java/io/github/tofpu/vertexlink/grpc/VertexLinkClient.java
+++ edge-node/src/main/java/io/github/tofpu/vertexlink/grpc/VertexLinkClient.java
@@ -4,36 +4,20 @@ import com.google.protobuf.ByteString;
 import io.github.tofpu.vertexlink.protos.TelemetryPayloadData;
 import io.github.tofpu.vertexlink.protos.VertexLinkServiceGrpc;
 import io.github.tofpu.vertexlink.telemetry.TelemetryPayload;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
+import io.github.tofpu.vertexlink.util.grpc.AbstractClient;
 import io.grpc.StatusRuntimeException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.net.ConnectException;
 import java.util.UUID;
 
-public class VertexLinkClient<T extends TelemetryPayload> {
-    private static final Logger log = LoggerFactory.getLogger(VertexLinkClient.class);
+public class VertexLinkClient<T extends TelemetryPayload> extends AbstractClient<
+        VertexLinkServiceGrpc.VertexLinkServiceBlockingStub,
+        VertexLinkServiceGrpc.VertexLinkServiceStub
+        > {
     private final GrpcDataAdapter<T> grpcDataAdapter;
 
-    private final ManagedChannel channel;
-    private final VertexLinkServiceGrpc.VertexLinkServiceBlockingStub blockingStub;
-    private final VertexLinkServiceGrpc.VertexLinkServiceStub asyncStub;
-
     public VertexLinkClient(String host, int port, GrpcDataAdapter<T> grpcDataAdapter) {
-        this(grpcDataAdapter, ManagedChannelBuilder.forAddress(host, port).usePlaintext());
-    }
-
-    /**
-     * Construct client for accessing RouteGuide server using the existing channel.
-     */
-    public VertexLinkClient(GrpcDataAdapter<T> grpcDataAdapter, ManagedChannelBuilder<?> channelBuilder) {
+        super(host, port, VertexLinkServiceGrpc::newBlockingStub, VertexLinkServiceGrpc::newStub);
         this.grpcDataAdapter = grpcDataAdapter;
-        this.channel = channelBuilder.build();
-        this.blockingStub = VertexLinkServiceGrpc.newBlockingStub(channel);
-        this.asyncStub = VertexLinkServiceGrpc.newStub(channel);
     }
 
     /**
@@ -43,7 +27,6 @@ public class VertexLinkClient<T extends TelemetryPayload> {
         TelemetryPayloadData request = TelemetryPayloadData.newBuilder()
                 .setNodeId(ByteString.copyFrom(nodeId.toString().getBytes()))
                 .setType(payload.type())
-//                .setTimestamp(payload.id().timestampInMs())
                 .setId(payload.id())
                 .setData(ByteString.copyFrom(grpcDataAdapter.serialize(payload)))
                 .build();
@@ -52,28 +35,8 @@ public class VertexLinkClient<T extends TelemetryPayload> {
             blockingStub.uploadTelemetry(request);
             return true;
         } catch (StatusRuntimeException e) {
-//            throw new RuntimeException(e);
-//            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-            if (isConnectionException(e)) {
-                log.error("Failed to upload telemetry: Cannot reach the server (Network/Connection issue).");
-            } else {
-                log.info("Failed to upload telemetry. RPC failed: {}", e.getStatus());
-            }
+            super.handleException(e);
             return false;
         }
-    }
-
-    private boolean isConnectionException(StatusRuntimeException e) {
-        // Connection drops and timeouts almost always fall under UNAVAILABLE
-        if (e.getStatus().getCode() == Status.Code.UNAVAILABLE) {
-            Throwable cause = e.getCause();
-            while (cause != null) {
-                if (cause instanceof ConnectException) {
-                    return true;
-                }
-                cause = cause.getCause(); // Move down the causal chain
-            }
-        }
-        return false;
     }
 }
