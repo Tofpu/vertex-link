236df638329e0ea6ad8f0f10594dcc76456fa7ba
Tofpu <tofpuu@gmail.com>
2026-06-29 14:01:37 +0300

    feat(common): introduce a standardized gRPC server implementation

--- common/src/main/java/io/github/tofpu/vertexlink/util/grpc/SimpleServer.java
+++ common/src/main/java/io/github/tofpu/vertexlink/util/grpc/SimpleServer.java
@@ -1,40 +1,36 @@
-package io.github.tofpu.vertexlink.grpc;
+package io.github.tofpu.vertexlink.util.grpc;
 
-import io.grpc.Grpc;
-import io.grpc.InsecureServerCredentials;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class VertexLinkServer {
+public class SimpleServer<T extends BindableService> {
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(VertexLinkServer.class);
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SimpleServer.class);
     private final int port;
-    private final Server server;
+    private final io.grpc.Server server;
 
     /** Create a RouteGuide server listening on {@code port}. */
-    public VertexLinkServer(int port, VertexLinkService<?> bindableService) {
-        this(Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create()), port, bindableService);
+    public SimpleServer(int port, T service) {
+        this(Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create()), port, service);
     }
 
     /** Create a RouteGuide server using serverBuilder as a base */
-    public VertexLinkServer(ServerBuilder<?> serverBuilder, int port, VertexLinkService<?> bindableService) {
+    public SimpleServer(ServerBuilder<?> serverBuilder, int port, T service) {
         this.port = port;
-        this.server = serverBuilder.addService(bindableService)
+        this.server = serverBuilder.addService(service)
                 .build();
     }
 
     public void start() throws IOException {
         server.start();
-//        logger.info("Server started, listening on " + port);
         log.info("Server started, listening on {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             // Use stderr here since the logger may have been reset by its JVM shutdown hook.
             System.err.println("*** shutting down gRPC server since JVM is shutting down");
             try {
-                VertexLinkServer.this.stop();
+                SimpleServer.this.stop();
             } catch (InterruptedException e) {
                 e.printStackTrace(System.err);
             }
