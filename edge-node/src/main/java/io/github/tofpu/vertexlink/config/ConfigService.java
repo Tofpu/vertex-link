1aa914d2c5ad77b715d1c4c868b2cd40d61768e0
Tofpu <tofpuu@gmail.com>
2026-06-30 16:11:39 +0300

    feat(edge-node): add validation checks in `ConfigService` to ensure that the default values are present

--- edge-node/src/main/java/io/github/tofpu/vertexlink/config/ConfigService.java
+++ edge-node/src/main/java/io/github/tofpu/vertexlink/config/ConfigService.java
@@ -1,18 +1,26 @@
 package io.github.tofpu.vertexlink.config;
 
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 public class ConfigService {
+    private static final String ROOT_PATH_NAME = "edge-node";
+
     private Config config;
     private final ConfigurationListener configurationListener;
 
     public ConfigService(Config config, ConfigurationListener configurationListener) {
-        this.config = config;
+        this.config = verifyValidity(config);
         this.configurationListener = configurationListener;
     }
 
+    public Config verifyValidity(Config config) {
+        config.checkValid(ConfigFactory.defaultReference(), ROOT_PATH_NAME);
+        return config;
+    }
+
     public void updateConfig(Config newConfig) {
-        this.config = newConfig;
+        this.config = verifyValidity(newConfig);
         this.configurationListener.onConfigurationUpdate(newConfig);
     }
 
