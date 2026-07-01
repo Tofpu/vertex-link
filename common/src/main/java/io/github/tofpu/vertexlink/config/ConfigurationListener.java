a9135aab9bc6d5a6929c9e20f338bb431ce62a0b
Tofpu <tofpuu@gmail.com>
2026-07-02 00:06:41 +0300

    feat(common): introduce `IDENTITY` static property in `ConfigurationListener` interface

--- common/src/main/java/io/github/tofpu/vertexlink/config/ConfigurationListener.java
+++ common/src/main/java/io/github/tofpu/vertexlink/config/ConfigurationListener.java
@@ -3,5 +3,7 @@ package io.github.tofpu.vertexlink.config;
 import com.typesafe.config.Config;
 
 public interface ConfigurationListener {
+    ConfigurationListener IDENTITY = newConfig -> {};
+
     void onConfigurationUpdate(Config newConfig);
 }
