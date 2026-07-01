a074ecd5afd2ce2f76c058aefc323473a2be1a4a
Tofpu <tofpuu@gmail.com>
2026-07-02 00:54:52 +0300

    feat: add `mockito` dependency in parent build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ subprojects {
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
         testImplementation("org.assertj:assertj-core:3.27.7")
+        testImplementation("org.mockito:mockito-core:5.23.0")
     }
 
     pitest {
